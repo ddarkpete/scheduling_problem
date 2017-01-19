806bd90703caab7a83135b2839e40449143e68ce
Piotr Michalak <pmichalak17@gmail.com>
2017-01-19 19:23:24 +0100

    zmiana struktury plików

---
+++ scheduing_fs_ts/Pause.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheduing_fs_ts
+{
+    class Pause
+    {
+        public int p_id;
+        public int p_start;
+        public int p_end;
+        public int p_duration;
+    }
+}
---
+++ scheduing_fs_ts/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace scheduing_fs_ts
+{
+    class Task
+    {
+        public int maszyna_op1;
+        public int duration_op1;
+        public int duration_op2;
+        public int maszyna_op2;
+        public int start;
+        //public int pause;
+    }
+}
