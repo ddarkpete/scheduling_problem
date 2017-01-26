11e477e9f0a18ecad968a6256a2520a46f47de23
Piotr Michalak <pmichalak17@gmail.com>
2017-01-26 16:12:37 +0100

    wczytywanie działa

--- scheduing_fs_ts/Form1.Designer.cs
+++ scheduing_fs_ts/Form1.Designer.cs
@@ -114,7 +114,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TabuSearch";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
